ef96d3d22ed1f00bde3667d2e8e6bba66052c9cb
vansh-31 <vanshgawra008@gmail.com>
2023-10-25 01:12:37 +0530

    Updated the BG Image of Hero Section

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
 const Event = ({ title, description, image }) => {
 	return (
-		<div className="w-full flex flex-col justify-center p-4">
-			<img src={image} alt="" />
-			<h3 className="font-bold text-lg" >{title}</h3>
-			<p className="text-gray-700" >{description}</p>
-		</div>
+		<a
+			href="https://9qqirfue454.typeform.com/to/tEQPxFfL"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<div className="w-full flex flex-col justify-center p-4">
+				<img src={image} alt="" />
+				<h3 className="font-bold text-lg">{title}</h3>
+				<p className="text-gray-700">{description}</p>
+			</div>
+		</a>
 	);
 };
 
