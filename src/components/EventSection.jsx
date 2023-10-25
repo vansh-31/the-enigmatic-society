d2b58e7e01705bd7b3f77c5692f70701b9787e6f
vansh-31 <vanshgawra008@gmail.com>
2023-10-25 23:10:09 +0530

    Replaced Headless UI Tabs with Chakra UI Tabs

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,43 +1,29 @@
-import React, { useState, useContext } from "react";
-import { Tab } from "@headlessui/react";
+import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import Events from "./Events";
+import { Tab, Tabs, TabList, TabPanels, TabPanel } from "@chakra-ui/react";
 const EventSection = () => {
 	const { upcoming_events, past_events } = useContext(AppContext);
-	const [tab, setTab] = useState(0);
 	return (
-		<div className="flex flex-col gap-y-6 my-20">
-			<h1 className="text-center text-5xl font-bold text-gray-800">Events</h1>
-			<div>
-				<Tab.Group>
-					<Tab.List className="flex justify-center gap-x-4">
-						<Tab
-							className={`px-10 py-2 text-lg rounded-lg  border-none outline-none ${
-								tab === 0
-									? "bg-[#00BDD6] text-white"
-									: "text-[#00BDD6] bg-white"
-							}`}
-							onClick={(e) => {
-								setTab(0);
-							}}
-						>
-							Upcoming
-						</Tab>
-						<Tab
-							className={`px-10 py-2 text-lg rounded-lg  border-none outline-none ${
-								tab ? "bg-[#00BDD6] text-white" : "text-[#00BDD6] bg-white"
-							}`}
-							onClick={(e) => {
-								setTab(1);
-							}}
-						>
-							Past Events
-						</Tab>
-					</Tab.List>
-				</Tab.Group>
+		<>
+			<div className="flex flex-col gap-y-6 my-20">
+				<h1 className="text-center text-5xl font-bold text-gray-800">Events</h1>
+				<Tabs isFitted isLazy variant="enclosed">
+					<TabList mb="1em">
+						<Tab>Upcoming Events</Tab>
+						<Tab>Past Events</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel>
+							<Events events={upcoming_events}></Events>
+						</TabPanel>
+						<TabPanel>
+							<Events events={past_events}></Events>
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
 			</div>
-			<Events events={tab === 0 ? upcoming_events : past_events}></Events>
-		</div>
+		</>
 	);
 };
 
