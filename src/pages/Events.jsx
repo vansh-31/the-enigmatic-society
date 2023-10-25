147a9be9c9ecd32f3c5df53ec8655b8d4236392a
vansh-31 <vanshgawra008@gmail.com>
2023-10-26 01:55:29 +0530

    Formatted the Events Page

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -8,27 +8,43 @@ const Events = () => {
 	const { upcoming_events } = useContext(AppContext);
 	return (
 		<div className="w-11/12 max-w-7xl mx-auto">
-			<h1 className="text-5xl font-bold my-8 uppercase">Upcoming Events</h1>
+			<h1 className="text-4xl font-bold my-8 uppercase">Upcoming Events</h1>
 			<Divider
 				marginBottom="10"
 				borderBottom="4px"
 				orientation="horizontal"
 			/>{" "}
-			<div className="flex flex-col gap-y-12" >
-				{upcoming_events.map((event, index) => (
-					<Card key={index} {...event}></Card>
-				))}
+			<div className="flex flex-col gap-y-12">
+				{upcoming_events.length === 0 ? (
+					<div className="flex justify-center items-center w-full min-h-[300px]">
+						<h1 className="text-5xl font-bold text-gray-400 text-opacity-70 my-8 uppercase">
+							Nothing to show
+						</h1>
+					</div>
+				) : (
+					upcoming_events.map((event, index) => (
+						<Card key={index} {...event}></Card>
+					))
+				)}
 			</div>
-			<h1 className="text-5xl font-bold my-8 uppercase">Past Events</h1>
+			<h1 className="text-4xl font-bold my-8 uppercase">Past Events</h1>
 			<Divider
 				marginBottom="10"
 				borderBottom="4px"
 				orientation="horizontal"
 			/>{" "}
-			<div className="flex flex-col gap-y-12" >
-				{past_events.map((event, index) => (
-					<Card key={index} {...event}></Card>
-				))}
+			<div className="flex flex-col gap-y-12">
+				{past_events.length === 0 ? (
+					<div className="flex justify-center items-center w-full min-h-[300px]">
+						<h1 className="text-5xl font-bold text-gray-400 text-opacity-70 my-8 uppercase">
+							Nothing to show
+						</h1>
+					</div>
+				) : (
+					past_events.map((event, index) => (
+						<Card key={index} {...event}></Card>
+					))
+				)}
 			</div>
 		</div>
 	);
